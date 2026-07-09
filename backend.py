a2d79ed16e5a8d7c1278447e64bf87b3cc4c94d4
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-07-09 20:21:37 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,2 +1,24 @@
 import pandas as pd
-data=pd.read_csv("Vanishing_Knowledge_Index_Dataset (2).csv")
+import fastapi as FastAPI
+data=pd.read_csv(https://github.com/omprakashpatna18-del/Vanishing_knowledge_index/blob/main/Vanishing_Knowledge_Index_Dataset%20(2).csv")
+app=FastAPI()
+#pulling out data
+categories=list(data["category"].str.strip().unique())
+practice
+@app.post("/search")
+def search_and_display(keywords):
+    keywords=keywords.split()
+  req_cat=None
+  for cat in catgories:
+    if any(word in keywords) in cat:
+      print("category found")
+      req_cat=cat
+      break
+  if req_cat not None:
+    options=data[data["category"]==req_cat].practice_name
+  for
+             
+     
+
+
+
