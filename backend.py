b75cf07c656ff1119e7606309eb9ef3381470f69
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-07-11 15:43:27 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Query, UploadFile, File, Form, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import pandas as pd
+from werkzeug.utils import secure_filename
 data=pd.read_csv("https://github.com/omprakashpatna18-del/Vanishing_knowledge_index/blob/da2cc3e9c110d69a84732251977338dc3d5243c2/Vanishing_Knowledge_Index_Dataset%20(2).csv")
 app=FastAPI()
 app.add_middleware(
@@ -56,6 +60,41 @@ async def data_retrieval(selected):
         "latitude":latitude,
         "longitude":longitude
         }
+#_-------adding files-----------#
+MEDIA_DIR = "./uploaded_media"
+LIVE_CSV_PATH = "live_dataset.csv"
+PENDING_CSV_PATH = "pending_submissions.csv"
+
+for folder in [MEDIA_DIR]:
+    os.makedirs(folder, exist_ok=True)
+columns = ["category", "practice_name", "region", "info", "science_check", "source_link", "media_asset"]
+for path in [LIVE_CSV_PATH,PENDING_CSV_PATH]:
+    if not os.path.exists(path):
+        pd.DataFrame(columns=columns).to_csv(path,index=False)
+
+security = HTTPBearer()
+ADMIN_TOKEN = "SuperSecretAdminKey123"
+ALLOWED_MIME_TYPES = ["audio/mpeg", "audio/wav", "audio/x-wav", "audio/ogg", "video/mp4", "video/webm"]
+
+def verify_admin(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if creditionals.creditionals!=ADMIN_TOKEN:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Administrative Authentication Token"
+        )
+    return True
+#-------Validation------------#
+class Data(BaseModel):
+    practice_name: str,
+    category: str,
+    region: str,
+    info: str,
+    source_link: file: UploadFile = File(...)
+#--------User Submission------#
+@app.post("/upload")
+async def upload(data: Data):
+    
+
+
         
     
     
